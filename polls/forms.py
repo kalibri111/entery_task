e59068811ba1d22ee2c7b81da6f5595ea243e517
kalibri111 <livan.skin@yandex.ru>
2020-02-17 14:42:34 +0300

    poll form refactors

--- polls/forms.py
+++ polls/forms.py
@@ -1,13 +1,12 @@
 from django import forms
 from .models import *
-from crispy_forms.helper import FormHelper
 
 
 class PoolChoiceForm(forms.Form):
-    choices = None
+    choices = forms.MultipleChoiceField(
+            choices=[],
+            widget=forms.CheckboxSelectMultiple(attrs={'class': '  custom-checkbox', 'type': 'checkbox'}))
 
     def __init__(self, data=None, *args, **kwargs):
         super(PoolChoiceForm, self).__init__(data)
-        self.fields['choices'] = forms.MultipleChoiceField(
-            choices=[(obj, obj) for obj in Choice.objects.filter(question__exact=kwargs['question'])],
-            widget=forms.CheckboxSelectMultiple(attrs={'class': '  custom-checkbox', 'type': 'checkbox'}))
+        self.fields['choices'].choices = Choice.objects.filter(question__exact=kwargs['question']).values_list('choice_text', 'choice_text')
